01426b781ac8d75888055deb25c9e558e7c2bab7
shriiix <shrighadi003@gmail.com>
2024-08-18 00:56:16 +0530

    fibo

---
+++ string/validpalindrome.cpp
@@ -0,0 +1,27 @@
+//input :- A man a plan ,a canal : panama
+//output :- amanaplanacanalpanama
+//with remove spaces.
+//check it is valid or not.
+
+#include<iostream>
+using namespace std;
+
+bool validPalindrome(string s){
+    
+
+     
+
+
+}
+
+
+
+int main(){
+    string s ="";
+    cout<<"Enter the String"<<endl;
+    cin>>s;
+    validPalindrome(s);
+
+
+
+}
